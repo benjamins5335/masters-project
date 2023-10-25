49d284686e00b6b206cc262f713c6e6cd1a744ce
benjamins5335 <ben_sanders@hotmail.com>
2023-10-25 01:03:42 +0100

    cxreate models folder for cnn

---
+++ cnn/models/binary_classifier.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class BinaryClassifier(nn.Module):
+    def __init__(self):
+        super(BinaryClassifier, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            nn.Flatten(),
+            nn.Linear(64 * 24 * 24, 128),  # adjusted for 96x96 input images
+            nn.ReLU(),
+            nn.Linear(128, 2),
+        )
+
+    
+    def forward(self, x):
+        return self.model(x)
+    
