25f4b9936ed9ed552536986ade8f683819672ff0
benjamins5335 <ben_sanders@hotmail.com>
2023-10-25 01:07:31 +0100

    update preprocessing file to crop to square then downsample

--- cnn/scripts/preprocess.py
+++ cnn/scripts/preprocess.py
@@ -1,14 +1,17 @@
 import cv2
 import os
+import argparse
+import time
 
-base_url = 'https://image-net.org/data/winter21_whole/{}.tar'
-base_path = '../data/real'
- 
-
-def process_images():
+def process_images(base_path):
+    print(f'Processing images in {base_path}...')
+    
     for root, dirs, files in os.walk(base_path):
+        
         for file in files:
-            if file.endswith('.JPEG'):
+            
+            if file.endswith('.JPEG') or file.endswith('.jpg') or file.endswith('.png') or file.endswith('.jpeg'):
+                print(f'Processing {file}...')
                 file_path = os.path.join(root, file)
                 img = cv2.imread(file_path)
                 
@@ -17,18 +20,23 @@ def process_images():
                     os.remove(file_path)
                     
                 elif width > 96 or height > 96:
-                    left = int((width - 96)/2)
-                    top = int((height - 96)/2)
-                    right = left + 96
-                    bottom = top + 96
+                    crop_size = min(width, height)
+                    left = (width - crop_size) // 2
+                    right = left + crop_size
+                    top = (height - crop_size) // 2
+                    bottom = top + crop_size
                     
                     img = img[top:bottom, left:right]
                     
                     img = cv2.resize(img, (96, 96), interpolation=cv2.INTER_LINEAR)
                     
                     cv2.imwrite(file_path, img)
-    
+               
                     
-                    
-                    
-                    
+if __name__ == "__main__":
+    print('Starting preprocessing...')
+    parser = argparse.ArgumentParser(description='Process images in a directory.')
+    parser.add_argument('base_path', type=str, help='Path to the base directory containing images to process.')
+    args = parser.parse_args()
+
+    process_images(args.base_path)
