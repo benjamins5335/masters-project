50e4c57478521ce8e0f0a39e29bea58c1fe35262
benjamins5335 <ben_sanders@hotmail.com>
2023-10-27 19:44:39 +0100

    add comments to cnn model

--- cnn/main.py
+++ cnn/main.py
@@ -7,29 +7,49 @@ from torch.utils.data import DataLoader
 
 from models.binary_classifier import BinaryClassifier
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from binary_classifier import BinaryClassifier
+
 def train_cnn():
+    """
+    Trains a convolutional neural network on a binary classification task.
+
+    Returns:
+    None
+    """
+    # Define data transformations
     transform = transforms.Compose([
         transforms.Resize((96, 96)),
         transforms.ToTensor(),
     ])
 
+    # Load dataset
     dataset = ImageFolder(root='test_data', transform=transform)
 
+    # Split dataset into train and test sets
     train_size = int(0.8 * len(dataset))
     test_size = len(dataset) - train_size
-
     train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
 
+    # Create data loaders
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-    
+
+    # Set device to GPU if available, otherwise CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Initialize model, loss function, and optimizer
     model = BinaryClassifier().to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    
+
+    # Train model
     num_epochs = 10
-    
     for epoch in range(num_epochs):
         model.train()
         for images, labels in train_loader:
@@ -40,13 +60,13 @@ def train_cnn():
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
         print(f'Epoch: {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}')
-        
+
+    # Evaluate model on test set
     model.eval()
     correct = 0
     total = 0
-    
     with torch.no_grad():
         for images, labels in test_loader:
             images = images.to(device)
@@ -55,9 +75,10 @@ def train_cnn():
             predicted = torch.round(outputs)
             total += labels.size(0)
             correct += (predicted == labels.unsqueeze(1)).sum().item()
-            
+
     print(f'Accuracy: {100 * correct / total}%')
-    
+
+    # Save model
     torch.save(model.state_dict(), 'model.pth')
     
 if __name__ == '__main__':
