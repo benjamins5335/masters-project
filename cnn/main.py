f3b46fd6ccbd1a0d4f1fb05ba0ca24237364ada1
benjamins5335 <ben_sanders@hotmail.com>
2023-11-17 00:40:27 +0000

    separate pytorch model into train and eval functions

--- cnn/main.py
+++ cnn/main.py
@@ -17,47 +17,37 @@ from torchvision.datasets import ImageFolder
 from models.binary_classifier import BinaryClassifier
 from PIL import Image
 
-def train_cnn():
+
+def train(model, train_loader, test_loader, num_epochs=10, lr=0.0001, device='cuda'):
     """
     Trains a convolutional neural network on a binary classification task.
+
+    Args:
+    - model: The model to be trained
+    - train_loader: DataLoader for training data
+    - test_loader: DataLoader for testing data
+    - num_epochs: Number of training epochs
+    - lr: Learning rate for the optimizer
+    - device: Device to use for training ('cuda' or 'cpu')
 
     Returns:
     None
     """
     
-    BATCH_SIZE = 16
+    # implement grid search for hyperparameter tuning
+    # define the grid search parameters
+    batch_size = [16, 32, 64]
+    epochs = [10, 20, 30]
+    learning_rate = [0.0001, 0.001, 0.01]
     
-    # Define data transformations (similar to ImageDataGenerator)
-    data_transforms = transforms.Compose([
-        transforms.ToTensor(),  # Convert images to tensors
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # used from imagenet
-    ])
     
-    full_ds = ImageFolder(root='data', transform=data_transforms)
-    
-    train_size = int(0.8 * len(full_ds))
-    test_size = len(full_ds) - train_size
-    
-    train_ds, test_ds = torch.utils.data.random_split(full_ds, [train_size, test_size])
-    
-    # Create data loaders
-    train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
-
-    # Set device to GPU if available, otherwise CPU
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(device)
-
-    # Initialize model, loss function, and optimizer
-    model = BinaryClassifier()
-    loss_fn = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
-    
-
+    # Set device
     model = model.to(device)
+    
+    # Initialize loss function and optimizer
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
-    # Train model
-    num_epochs = 10
     for epoch in range(num_epochs):
         print(f'Epoch: {epoch + 1}/{num_epochs}')
         model.train()
@@ -65,40 +55,54 @@ def train_cnn():
         train_acc = 0
         for images, labels in train_loader:
             images = images.to(device)
-            labels = labels.to(device).float()  # Ensure labels are floats
+            labels = labels.to(device).float()
             optimizer.zero_grad()
-            outputs = model(images).squeeze()  # Squeeze the output to remove the extra dimension
+            outputs = model(images).squeeze()
             loss = loss_fn(outputs, labels)
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-            predicted = torch.round(torch.sigmoid(outputs))  # Apply sigmoid and round to get predictions
+            predicted = torch.round(torch.sigmoid(outputs))
             train_acc += (predicted == labels).sum().item()
 
-            
-        print(f'Train loss: {train_loss / len(train_ds)}')
-        print(f'Train accuracy: {train_acc / len(train_ds)}')
-        
-        model.eval()
-        test_loss = 0
-        test_acc = 0
-        for images, labels in test_loader:
-            images = images.to(device)
-            labels = labels.to(device).float()
-            outputs = model(images).squeeze()
-            loss = loss_fn(outputs, labels)
-            test_loss += loss.item()
-            predicted = torch.round(torch.sigmoid(outputs))
-            test_acc += (predicted == labels).sum().item()
-            
-        print(f'Test loss: {test_loss / len(test_ds)}')
-        print(f'Test accuracy: {test_acc / len(test_ds)}')
-        print()
-        
+        print(f'Train loss: {train_loss / len(train_loader.dataset)}')
+        print(f'Train accuracy: {train_acc / len(train_loader.dataset)}')
+
+        evaluate(model, test_loader, device)
 
     # Save model
     torch.save(model.state_dict(), 'model.pth')
+    
+               
 
+def evaluate(model, data_loader, device='cuda'):
+    """
+    Evaluates a model on a given dataset.
+
+    Args:
+    - model: The model to be evaluated
+    - data_loader: DataLoader for the evaluation data
+    - device: Device to use for evaluation ('cuda' or 'cpu')
+
+    Returns:
+    None
+    """
+    model.eval()
+    loss_fn = nn.BCEWithLogitsLoss()
+    test_loss = 0
+    test_acc = 0
+    for images, labels in data_loader:
+        images = images.to(device)
+        labels = labels.to(device).float()
+        outputs = model(images).squeeze()
+        loss = loss_fn(outputs, labels)
+        test_loss += loss.item()
+        predicted = torch.round(torch.sigmoid(outputs))
+        test_acc += (predicted == labels).sum().item()
+
+    print(f'Test loss: {test_loss / len(data_loader.dataset)}')
+    print(f'Test accuracy: {test_acc / len(data_loader.dataset)}')
+    print()
 
 def inference_cnn():
     """
@@ -130,5 +134,25 @@ def inference_cnn():
     print(f'Predicted: {predicted}')
     
 if __name__ == '__main__':
-    train_cnn()
-    # inference_cnn()
+    BATCH_SIZE = 16
+
+    data_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    full_ds = ImageFolder(root='data', transform=data_transforms)
+    train_size = int(0.8 * len(full_ds))
+    test_size = len(full_ds) - train_size
+    train_ds, test_ds = torch.utils.data.random_split(full_ds, [train_size, test_size])
+
+    train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Initialize model
+    model = BinaryClassifier()
+
+    # Train the model
+    train(model, train_loader, test_loader, num_epochs=10, lr=0.0001, device=device)
