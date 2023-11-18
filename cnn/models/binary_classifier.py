9a903d46372bdad568afefbd8cafb5b98e80c73a
benjamins5335 <ben_sanders@hotmail.com>
2023-11-18 17:48:52 +0000

    update binary classifier model to include optional dropout rate

--- cnn/models/binary_classifier.py
+++ cnn/models/binary_classifier.py
@@ -8,20 +8,23 @@ from torchvision.datasets import ImageFolder
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class BinaryClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self, dropout=0.0):
         super(BinaryClassifier, self).__init__()
         self.model = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(p=dropout),
             
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            
+            nn.Dropout(p=dropout),
+
             nn.Flatten(),
             nn.Linear(64 * 24 * 24, 128),  # adjusted for 96x96 input images
             nn.ReLU(),
+            nn.Dropout(p=dropout),
             nn.Linear(128, 1),
         )
 
