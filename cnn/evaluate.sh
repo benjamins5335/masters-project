5f78873bf99f89601e667a9196e396b5a95ae1db
benjamins5335 <ben_sanders@hotmail.com>
2024-01-08 00:13:26 +0000

    edit evaluate.sh file to omit redundant --model_path flag

--- cnn/evaluate.sh
+++ cnn/evaluate.sh
@@ -1,6 +1,6 @@
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 --model_path <model_name>"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <model_name>"
     exit
 fi
 
-python3 evaluate.py --model_path $2 --whole_set --subclasses
+python3 evaluate.py --model_path $1 --whole_set --subclasses
